a9d7624ab28c9fbee4d750bdff2368bcb3a14a1b
chhatrapatikumar <kumarchhatrapati07@gmail.com>
2026-06-27 23:02:30 +0530

    removed apis from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-"use client";
-
-type users = {
-  id: number;
-  name: string;
-};
-
-export default async function Home() {
-  const baseUrl =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-
-  const res = await fetch(`${baseUrl}/api/users`);
-
-  if (!res.ok) {
-    return <div>Failed to load users. Status: {res.status}</div>;
-  }
-
-  const data: users[] = await res.json();
-
+export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen">
-      {data.map((it) => (
-        <div key={it.id}>
-          <h1 className="text-xl font-bold">{it.name}</h1>
-        </div>
-      ))}
+      <h1>Hello Next!</h1>
     </div>
   );
 }
